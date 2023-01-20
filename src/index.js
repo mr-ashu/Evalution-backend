628a24736807d5a29d266dd92dd3662b89bd4054
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2023-01-21 02:31:03 +0530

    login-signup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
  
-const userRouter=require("./src/Routes/user.routes") 
-const connect = require("./src/Config/db")
+const userRouter=require("./Routes/user.routes") 
+const connect = require("./Config/db")
 const PORT = process.env.PORT || 3000
 
 const app = express();
